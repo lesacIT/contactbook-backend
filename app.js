25fb0bcc9097fb2232712e89fb58dfb1f4514a57
LeSac <107132448+lesacIT@users.noreply.github.com>
2023-09-03 20:15:47 +0700

    "Dinh nghia cac route cho tai nguyen Contact"

--- app.js
+++ app.js
@@ -1,27 +1,22 @@
-// Export module với module.exports
-// Import module với require function.
 const express = require("express");
-
-// Sử dụng thư viện cors để trao đổi dữ liệu chéo từ client đến server
 const cors = require("cors");
 
+const contactsRouter = require("./app/routes/contact.route");
 
-// Gọi đến thư viện express và sử dụng nó
+
 const app = express();
 
 
-// Sử dụng cors
 app.use(cors());
-
-// Sử dụng express.json
 app.use(express.json());
 
-// Gọi router get
+app.use("/api/contacts", contactsRouter);
+
+
+// handle 404 response
+
+
 app.get("/", (req, res) => {
-    res.json({
-        message: "Welcome to contact book application. http://localhost:3000/"
-    });
-})
-
-
+    res.json({ message: "Wellcome to contact book application."});
+});
 module.exports = app;
--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const app = require("./app");
-
 const config = require("./app/config");
 
-// Start server
 const PORT = config.app.port;
-
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
+    console.log(`Sever is running on port ${PORT}.`);
 });
